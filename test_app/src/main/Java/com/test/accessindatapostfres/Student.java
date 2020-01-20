23e0d72da98061b47cf6b7a0611d62c5b9749a85
ArtemBorejko <44895050+ArtemBorejko@users.noreply.github.com>
2020-01-20 11:42:54 +0200

    Update Student.java

--- test_app/src/main/Java/com/test/accessindatapostfres/Student.java
+++ test_app/src/main/Java/com/test/accessindatapostfres/Student.java
@@ -2,7 +2,6 @@ package com.test.accessindatapostfres;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
