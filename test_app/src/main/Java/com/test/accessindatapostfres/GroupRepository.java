e19559593c9f22c0ce8445319c8e72988f052fde
ArtemBorejko <44895050+ArtemBorejko@users.noreply.github.com>
2020-01-21 17:15:38 +0200

    Delete GroupRepository.java

--- test_app/src/main/Java/com/test/accessindatapostfres/GroupRepository.java
+++
@@ -1,8 +0,0 @@
-package com.test.accessindatapostfres;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface GroupRepository extends CrudRepository<Group, Integer> {
-
-    Group findByName(String name);
-}
